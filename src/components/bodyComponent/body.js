3bc1d4000b6b02d5fc17a3730b4baf6a2378064f
ajmalzada <ajmalzada@yahoo.com>
2017-11-20 11:13:04 -0500

    Add files via upload

---
+++ src/components/bodyComponent/body.js
@@ -0,0 +1,228 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <body>
+<div className="container-fluid">
+
+     <div className="row">
+
+         <div className="col-md-10">
+
+            <div className="blogpost">
+
+            <div class="container">
+
+<div class="row">
+
+    <div class="col-md-1"></div>
+
+	<div class="col-md-8">
+
+		<div id="postlist">
+
+			<div class="panel">
+
+                <div class="panel-heading">
+
+                        <div class="row">
+
+                            <div class="column-fluid">
+
+                                <h1>My first favorite class @ Pace university </h1>
+
+                            </div>
+
+                            <div class="col-sm-2">
+
+                                <h5 class="pull-down">
+
+                                <small><em>11/12/2017</em></small>
+
+                                </h5>
+
+                            </div>
+
+                        </div>
+
+                  
+
+                </div>
+
+                
+
+            <div >
+
+            Mobile app devalopment was my first favorite class at Pace University. It was very intresting working with Android Studio
+
+            </div>
+
+            </div>
+
+            </div>
+
+            </div>
+
+            </div>
+
+            </div>
+
+            </div>
+
+            <p></p>
+
+            <div className="blogpost">
+
+            <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-1"></div>
+
+              <div class="col-md-8">
+
+                <div id="postlist">
+
+                  <div class="panel">
+
+                            <div class="panel-heading">
+
+                                    <div class="row">
+
+                                        <div class="column-fluid">
+
+                                            <h1 id='november'>My second favorite class @ Pace University </h1>
+
+                                        </div>
+
+                                        <div class="col-sm-2">
+
+                  
+
+                                            <h5 class="pull-down">
+
+                                            <small><em>11/13/2017</em></small>
+
+                                            </h5>
+
+                                        </div>
+
+                                    </div>
+
+                              
+
+                            </div>
+
+                            
+
+                        <div>
+
+                        My second favorite class at face university was CS 504 where I have learned advanced Java 
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+            <p></p>
+
+            <div className="blogpost">
+
+            <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-1"></div>
+
+              <div class="col-md-8">
+
+                <div id="postlist">
+
+                  <div class="panel">
+
+                            <div class="panel-heading">
+
+                                    <div class="row">
+
+                                        <div class="column-fluid">
+
+                                            <h1 id='june'>My third favorite class @ Pace University </h1>
+
+                                        </div>
+
+                                        <div class="col-sm-2">
+
+                                            <h5 class="pull-down">
+
+                                            <small><em>11/13/2017</em></small>
+
+                                            </h5>
+
+                                        </div>
+
+                                    </div>
+
+                              
+
+                            </div>
+
+                            
+
+                        <div>
+
+                        My third favorite class at Pace university was Concept and Structer of internet Computing. I have really enjoyed and working with html and css.
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+                        </div>
+
+         </div>
+
+         <div className="col-md-2">
+
+         <div id="links">
+
+            <ul>
+
+       <li><a href="http://facebook.com/ajmalzada">Facebook</a></li>
+       <li><a href="http://twitter.com">Twitter</a></li>
+       <li><a href="http://github.com">Github</a></li>
+              </ul>
+
+         </div>
+
+
+
+         </div>
+
+     </div>
+
+ </div>
+</body>
+
+
+    );
+  }
+}
+
+export default Body;
---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+     <header>
+       <div className="container-fluid">
+        <center> React Js App<br/>
+        Welcome to my blog</center>
+       </div>
+
+         </header>
+
+    );
+  }
+}
+
+export default Header;
